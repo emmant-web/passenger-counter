b26b45c8bfa2f6f7ed037d2b87ff072d5d928225
Emmanuel Toribio <emmanueldtoribio@gmail.com>
2025-04-04 02:49:41 +0800

    first version of passenger counter app

---
+++ index.js
@@ -0,0 +1,19 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+  // count = count + 1;
+
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let saveNum = ` ${count} - `;
+
+  saveEl.textContent = saveEl.textContent + saveNum;
+
+  count = 0;
+  countEl.textContent = 0;
+}
